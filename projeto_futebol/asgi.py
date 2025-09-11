94ef7812c85bd07513d4982cb38922b6424e7fa1
Fernando Freitas <fernandofreitassud2016@gmail.com>
2025-09-11 10:45:29 -0300

    O primeiro commit desse projeto django para testar se está tudo em ordem

---
+++ projeto_futebol/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for projeto_futebol project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto_futebol.settings')
+
+application = get_asgi_application()
