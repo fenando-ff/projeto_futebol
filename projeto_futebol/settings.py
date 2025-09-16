d2f517cd3f9be2c2cd13b850871cdc2805e18deb
Fernando Freitas <fernandofreitassud2016@gmail.com>
2025-09-16 00:12:52 -0300

    requirements.txt com novas bibliotecas para usar o banco

--- projeto_futebol/settings.py
+++ projeto_futebol/settings.py
@@ -78,14 +78,14 @@ WSGI_APPLICATION = 'projeto_futebol.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('DATABASE_ENGINE'),
-        'NAME': os.environ.get('DATABASE_NAME'),
-        'USER': os.environ.get('DATABASE_USER'),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        'HOST': os.environ.get('DATABASE_HOST'),
-        'PORT': os.environ.get('DATABASE_PORT'),
+        'ENGINE': os.environ.get('DB_ENGINE'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
         'OPTIONS': {
-            'SSL': {
+            'ssl': {
                 'ca': os.path.join(BASE_DIR, os.environ.get("DB_SSL_CA")), # type: ignore
             }
         }
