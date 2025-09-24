051aec36675a205be96b6729ed1258a8064f5207
Fernando Freitas <fernandofreitassud2016@gmail.com>
2025-09-24 19:46:19 -0300

    mudança de local do HttpResponse no views.py

--- app_futebol/views.py
+++ app_futebol/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-from django.http import HttpResponse
 
 def home(request):
     return render(request, "app_futebol/index.html")
