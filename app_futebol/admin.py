2353e3d396ffeb224bab7fbfe029c89b3636b70f
Pedro Silva <pedro56438076@edu.pa.senac.br>
2025-11-24 14:35:50 -0300

    atualizando

--- app_futebol/admin.py
+++ app_futebol/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
+
 
 # Register your models here.
+admin.site.register(models.Clientes)
+admin.site.register(models.Produtos)
+admin.site.register(models.Funcionarios)
+admin.site.register(models.Pedido)
