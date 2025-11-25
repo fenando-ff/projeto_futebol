3f95281aa0c20a869e8ea5dc6eed01317d54117a
Fernando Freitas <fernandofreitassud2016@gmail.com>
2025-11-24 23:57:54 -0300

    Cadastro de clientes falhando
    Erro de categoria de clientes no banco

--- accounts/views.py
+++ accounts/views.py
@@ -8,48 +8,48 @@ from app_futebol import models
 
 # fique um tempao batendo cabeca com essa porra aqui, peguei algumas coisa do chat
 
-def register_user(request):
-    if request.method == 'POST':
+# def register_user(request):
+#     if request.method == 'POST':
 
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        sexo = request.POST.get('sexo')
-        cpf = request.POST.get('cpf')
-        telefone = request.POST.get('telefone')
+#         username = request.POST.get('username')
+#         email = request.POST.get('email')
+#         password = request.POST.get('password')
+#         sexo = request.POST.get('sexo')
+#         cpf = request.POST.get('cpf')
+#         telefone = request.POST.get('telefone')
 
-        # Verificando se o burro do usuário já existe
-        if User.objects.filter(username=username).exists():
-            messages.error(request, 'Usuário já existe!')
-            return redirect('register')
+#         # Verificando se o burro do usuário já existe
+#         if User.objects.filter(username=username).exists():
+#             messages.error(request, 'Usuário já existe!')
+#             return redirect('register')
 
-        if User.objects.filter(email=email).exists():
-            messages.error(request, 'Email já está em uso!')
-            return redirect('register')
+#         if User.objects.filter(email=email).exists():
+#             messages.error(request, 'Email já está em uso!')
+#             return redirect('register')
 
-        if Perfil.objects.filter(cpf=cpf).exists():
-            messages.error(request, 'CPF já está cadastrado!')
-            return redirect('register')
+#         if Perfil.objects.filter(cpf=cpf).exists():
+#             messages.error(request, 'CPF já está cadastrado!')
+#             return redirect('register')
 
-        # Criando o burro do usuário
-        user = User.objects.create_user(
-            username=username,
-            email=email,
-            password=password
-        )
+#         # Criando o burro do usuário
+#         user = User.objects.create_user(
+#             username=username,
+#             email=email,
+#             password=password
+#         )
 
-        # adicionando os bagulho do perfil com os dados extras
-        Perfil.objects.create(
-            user=user,
-            sexo=sexo,
-            cpf=cpf,
-            telefone=telefone
-        )
+#         # adicionando os bagulho do perfil com os dados extras
+#         Perfil.objects.create(
+#             user=user,
+#             sexo=sexo,
+#             cpf=cpf,
+#             telefone=telefone
+#         )
 
-        messages.success(request, 'Conta criada com sucesso!')
-        return redirect('login')
+#         messages.success(request, 'Conta criada com sucesso!')
+#         return redirect('login')
 
-    return render(request, 'acconts/cadastro.html')
+#     return render(request, 'acconts/cadastro.html')
 
 
 # -------------------- codigo pra estudar e aplicar ----------------------
@@ -59,8 +59,8 @@ def cadastro(request):
         nome = request.POST.get("nome")
         sobrenome = request.POST.get("sobrenome")
         email = request.POST.get("email")
-        cpf = request.POST.get("cpf")
         telefone = request.POST.get("telefone")
+        cpf = request.POST.get("cpf")
         senha = request.POST.get("senha")
         sexo = request.POST.get("sexo")
 
