bba3529178835a9b8dc5d90ef5101598cba4315e
Pedro-Daniel223 <danicc223@icloud.com>
2026-01-14 22:00:17 -0300

    conexão com aiven estabelecida, porém erro nos settigns

--- projeto_futebol/settings.py
+++ projeto_futebol/settings.py
@@ -92,12 +92,13 @@ DATABASES = {
     #     'HOST': os.environ.get('DB_HOST'),
     #     'PORT': os.environ.get('DB_PORT'),
     # }
-    'default': {
-        dj_database_url.config(
-            default=os.environ.get('DATABASE_URL'),
-            conn_max_age=600,
+    
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
         )
-    }
+    
 }
 
 # Confiurações do email
