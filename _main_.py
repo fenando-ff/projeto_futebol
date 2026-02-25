763a1d127804dafe8271e01bde41afc58cd5d72b
Nayane <cnayane462@gmail.com>
2026-02-25 15:00:15 -0300

    teste render

--- _main_.py
+++ _main_.py
@@ -1,10 +1,13 @@
+
+from app import App 
+
 if __name__ == "__main__":
     try:
+        # Agora o Python reconhece o que é App
         app = App()
         app.mainloop()
     except Exception as e:
         print(f"Erro ao iniciar GUI (esperado no Render): {e}")
-        # Mantenha o processo vivo para o Render não dar erro de saída
         import time
         while True:
             time.sleep(3600)
