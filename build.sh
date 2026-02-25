376597abe3c065d03b4bb65e8bbdc479cb4e0354
Nayane <cnayane462@gmail.com>
2026-02-25 16:40:50 -0300

    Fix: removing old main and updating settings

--- build.sh
+++ build.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
-# Sair se houver erro
 set -o errexit
-
-# Instalar as bibliotecas
 pip install -r requirements.txt
-
-# Coletar arquivos estáticos (CSS/JS)
 python manage.py collectstatic --no-input
-
-# Rodar as migrações do banco de dados
 python manage.py migrate
