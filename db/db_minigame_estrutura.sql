4e703af3186cca99f43425aace4de35c821c96a6
Fernando Freitas <fernandofreitassud2016@gmail.com>
2026-04-29 16:59:28 -0300

    Scripts do banco de dados minigame

--- db/db_minigame_estrutura.sql
+++ db/db_minigame_estrutura.sql
@@ -2,7 +2,7 @@ CREATE DATABASE  IF NOT EXISTS `db_minigame` /*!40100 DEFAULT CHARACTER SET utf8
 USE `db_minigame`;
 -- MySQL dump 10.13  Distrib 8.0.46, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: db_minigame
+-- Host: localhost    Database: db_minigame
 -- ------------------------------------------------------
 -- Server version	8.0.31
 
@@ -37,16 +37,6 @@ CREATE TABLE `alternativas` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `alternativas`
---
-
-LOCK TABLES `alternativas` WRITE;
-/*!40000 ALTER TABLE `alternativas` DISABLE KEYS */;
-INSERT INTO `alternativas` VALUES (1,'Coritiba',0,0,1),(2,'Palmeiras',1,10,1),(3,'Goiás',0,0,1),(4,'Guarani',0,0,1),(5,'Mineirão',0,0,2),(6,'Beira-Rio',0,0,2),(7,'Maracanã',1,10,2),(8,'Neo Química Arena',0,0,2),(9,'Bahia',0,0,3),(10,'Grêmio',1,10,3),(11,'São Paulo',0,0,3),(12,'Fluminense',0,0,3),(13,'Santos',1,10,4),(14,'Corinthians',0,0,4),(15,'Botafogo',0,0,4),(16,'Vitória',0,0,4),(17,'Flamengo',0,0,5),(18,'Cruzeiro',0,0,5),(19,'Palmeiras',1,10,5),(20,'Vasco',0,0,5),(21,'Curitiba',0,0,6),(22,'Porto Alegre',1,10,6),(23,'Florianópolis',0,0,6),(24,'Belo Horizonte',0,0,6),(25,'Fluminense',1,10,7),(26,'Botafogo',0,0,7),(27,'Bragantino',0,0,7),(28,'Athletico-PR',0,0,7),(29,'Cruzeiro',0,0,8),(30,'América-MG',0,0,8),(31,'Atlético-MG',1,10,8),(32,'Cuiabá',0,0,8),(33,'Tubarão',0,0,9),(34,'Leão',1,10,9),(35,'Raposa',0,0,9),(36,'Galo',0,0,9),(37,'São Paulo',1,10,10),(38,'Juventude',0,0,10),(39,'Criciúma',0,0,10),(40,'Corinthians',0,0,10);
-/*!40000 ALTER TABLE `alternativas` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `participantes`
 --
 
@@ -57,19 +47,9 @@ CREATE TABLE `participantes` (
   `id_participante` int NOT NULL,
   `nome_participante` varchar(45) NOT NULL,
   `tempo` time DEFAULT NULL,
-  `total_pontos` int DEFAULT NULL,
   PRIMARY KEY (`id_participante`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `participantes`
---
-
-LOCK TABLES `participantes` WRITE;
-/*!40000 ALTER TABLE `participantes` DISABLE KEYS */;
-/*!40000 ALTER TABLE `participantes` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `questoes`
@@ -84,16 +64,6 @@ CREATE TABLE `questoes` (
   PRIMARY KEY (`id_questao`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `questoes`
---
-
-LOCK TABLES `questoes` WRITE;
-/*!40000 ALTER TABLE `questoes` DISABLE KEYS */;
-INSERT INTO `questoes` VALUES (1,'Qual time é conhecido como \"Verdão\" e joga no Allianz Parque?'),(2,'Em qual estádio o Flamengo e o Fluminense costumam mandar seus jogos?'),(3,'Qual destes times é conhecido como \"Imortal Tricolor\"?'),(4,'Qual time paulista tem o apelido de \"Peixe\" e revelou o Pelé?'),(5,'Qual é o time que possui o maior número de títulos do Campeonato Brasileiro até 2024?'),(6,'O Sport Club Internacional pertence a qual cidade?'),(7,'Qual time é o atual campeão da Copa Libertadores da América (2023)?'),(8,'Qual time mineiro joga na Arena MRV?'),(9,'Qual é o mascote oficial do Fortaleza Esporte Clube?'),(10,'Qual time é conhecido como o \"Clube da Fé\" e joga no MorumBIS?');
-/*!40000 ALTER TABLE `questoes` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `respostas`
@@ -115,15 +85,6 @@ CREATE TABLE `respostas` (
   CONSTRAINT `fk_participantes_questoes_alternativas1` FOREIGN KEY (`alternativa_id`) REFERENCES `alternativas` (`id_alternativa`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `respostas`
---
-
-LOCK TABLES `respostas` WRITE;
-/*!40000 ALTER TABLE `respostas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `respostas` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -134,4 +95,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-04-27 17:47:25
+-- Dump completed on 2026-04-29 16:53:06
