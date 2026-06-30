4a7bee7fc7320c79d73a5c657b20f93c325aed03
Fernando Freitas <fernandofreitassud2016@gmail.com>
2026-06-30 17:57:14 -0300

    Refactor(url): ajuste das rotas da api agora é 127.0.0.1:8000/swagger/

--- projeto_futebol/urls.py
+++ projeto_futebol/urls.py
@@ -35,7 +35,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('', include('app_futebol.urls')),
     path('api/', include('app_futebol.urls_api')),
