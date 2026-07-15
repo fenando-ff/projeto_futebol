c955df4a3f1bbe1b56231f6b4b347a6f01bba2a7
Pedro-Daniel223 <danicc223@icloud.com>
2026-07-15 19:28:05 -0300

    Update settings.py
    atualizando para meu pc de casa

--- projeto_futebol/settings.py
+++ projeto_futebol/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # DEBUG dinâmico: False no Render, True local
 DEBUG = os.environ.get('RENDER', 'False') == 'True' or os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "projeto-futebol.onrender.com", ".onrender.com", "10.20.83.22", "192.168.61.90"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "projeto-futebol.onrender.com", ".onrender.com", "10.20.83.22", "192.168.61.90","192.168.1.8"]
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fallback-key')
 
@@ -122,3 +122,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # print("DB_HOST local =", os.environ.get("DB_HOST_LOCAL"))
 print("Host selecionado =", DATABASES["default"]["HOST"])
 # print("Engine escolhida =", DATABASES["default"]["ENGINE"])
+
+# para iniciar o servidor do Api mobile do iphone
+# python manage.py runserver 0.0.0.0:8000
